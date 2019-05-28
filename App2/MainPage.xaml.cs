6025d23bb9014dfab2d84460299a06b887dc00bb
ShreyalAcharya <51136352+ShreyalAcharya@users.noreply.github.com>
2019-05-29 01:51:50 +0530

    Add files via upload

---
+++ App2/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public string Format { get; set; }
+        public DateTime? SelectedDate;
+
+        
+        public Color ResetColor
+        {
+            set => LblReset.TextColor = value;
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Text_OnFocused(object sender, FocusEventArgs e)
+        {
+            TextEntry.Unfocus();
+            Date.Focus();
+        }
+
+        private void Date_OnDateSelected(object sender, DateChangedEventArgs e)
+        {
+            SelectedDate = e.NewDate;
+            //TextEntry.Text = String.IsNullOrEmpty(Format) ? e.NewDate.ToString(Format) : e.NewDate.ToString();
+            TextEntry.Text = String.IsNullOrEmpty(Format) ? Convert.ToDateTime(e.NewDate).ToShortDateString() : Convert.ToDateTime(e.NewDate).ToShortDateString();
+        }
+
+        private void Reset_OnTapped(object sender, EventArgs e)
+        {
+            SelectedDate = null;
+            TextEntry.Text = String.Empty;
+        }
+    }
+}
